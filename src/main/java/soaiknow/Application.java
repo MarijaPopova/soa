d4ff17c7f789a50dc9b2ee79e2f7f0171057894f
MarijaPopova <popovam@outlook.com>
2017-04-18 17:58:12 +0200

    changing docker files

--- src/main/java/soaiknow/Application.java
+++ src/main/java/soaiknow/Application.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
+
 
 /**
  * Created by 131513 on 3/7/2017.
@@ -12,6 +14,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 
 @ComponentScan
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
     @Override
